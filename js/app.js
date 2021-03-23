658f111123a5e7344af08d130aba2a5878821a88
noureddein <noureddein@gmail.com>
2021-03-23 18:57:11 +0200

    lab03 Not Solved yet

--- js/app.js
+++ js/app.js
@@ -7,54 +7,59 @@ function Pics(img) {
   this.keyword = img.keyword;
   this.horns = img.horns;
 }
-
+let newObjectItems = [];
 Pics.prototype.render = function () {
-  let $imgTemplet = $('#photoTemplate').clone();
-  $imgTemplet.addClass(`${this.keyword}`);
-  $('main').append($imgTemplet);
-  $imgTemplet.find('h2').text(this.title);
-  $imgTemplet.find('img').attr({
-    src: this.image_url,
-    title: this.title,
-  });
-  $imgTemplet.find('p').text(this.description);
+  let $imgTemplet = $('#photoTemplate').html();
+  // $imgTemplet.addClass(`${this.keyword}`);
+  // $('main').append($imgTemplet);
+  // $imgTemplet.find('h2').text(this.title);
+  // $imgTemplet.find('img').attr({
+  //   src: this.image_url,
+  //   title: this.title,
+  // });
+  // $imgTemplet.find('p').text(this.description);
+  let html = Mustache.render($imgTemplet, this);
+  return html;
 };
-
 const keywordArray = [];
 Pics.prototype.renderByKeyword = function () {
-  console.log(keywordArray);
   keywordArray.forEach(item => {
     let $selectEl = $(`<option value=${item}>${item}</option>`);
     $('select').append($selectEl);
   });
 };
 
+// Pages Array for chage between pages
+const pages = ['page', 'page-2'];
 
 Pics.readJson = () => {
   const ajaxSettings = {
     method: 'get',
     dataType: 'json'
   };
-  let pictures;
-  $.ajax('./../data/page.json', ajaxSettings)
-    .then(page => {
-      page.forEach(item => {
-        pictures = new Pics(item);
-        if (!keywordArray.includes(pictures.keyword)) {
-          keywordArray.push(pictures.keyword);
-        }
-        pictures.render();
-
-      });
-      pictures.renderByKeyword();
+  $.ajax(`../data/${pages[1]}.json`, ajaxSettings).then(page => {
+    let optionsList;
+    //Push Items From JSON File to the newObjectItems Array
+    page.forEach(item => {
+      newObjectItems.push(optionsList = new Pics(item));
+      if (!keywordArray.includes(item.keyword)) {
+        keywordArray.push(item.keyword);
+      }
     });
+    optionsList.renderByKeyword();
 
+    //Renderd Items
+    $('#clickToRenderPage1').click(function () {
+      newObjectItems.forEach(item => {
+        $('#showRenderdItems').append(item.render());
+      });
+    });
+  });
 
 };
 $(() => Pics.readJson());
 
 $(document).ready(function () {
-
   $('#imgSelet').change(function () {
     if ($(this).val() === 'default') {
       $('.a').show();
@@ -63,5 +68,4 @@ $(document).ready(function () {
       $('.' + $(this).val()).show();
     }
   });
-
 });
